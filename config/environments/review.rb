076587ad89dfba9dad33b68561daf2102aab416f
Sophie Lamothe <smlamothe@tamu.edu>
2025-09-17 15:16:21 -0500

    Update review.rb

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -1,17 +1,11 @@
 require_relative "production"
 
 Rails.application.configure do
-  # review apps should behave like production but not require credentials
+  # behave like production but don't require credentials
   config.require_master_key = false
 
-  # make sure logs go to STDOUT on Heroku (production.rb already wires this
-  # when RAILS_LOG_TO_STDOUT is present, which you’ve set)
-  config.log_level = :info
-
-  # keep eager load on (good for dynos)
+  # production-like defaults for review apps
   config.eager_load = true
-
-  # serve static files if the env var is set (you set RAILS_SERVE_STATIC_FILES)
+  config.log_level = :info
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 end
-RUBY
